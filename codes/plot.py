55bbd885799e60a2bd6c4bed35b38f20dbffdb87
Ashraf-1508 <ayaanashraf69@gmail.com>
2024-01-20 16:29:31 +0530

    figs codes added

---
+++ codes/plot.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+# Generate data points
+n_values = list(range(0,22))
+y_values = [6 + 6 * n for n in n_values]
+
+# Create a stem plot
+plt.stem(n_values, y_values, linefmt='-', markerfmt='o', basefmt=' ')
+
+# Add labels and title
+plt.xlabel('n')
+plt.ylabel('6 + 6n')
+plt.title('Stem Plot of 6 + 6n vs n')
+
+# Display the plot
+plt.show()
+
