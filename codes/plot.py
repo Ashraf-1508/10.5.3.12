8289466e64a4f8234ffb533898717dacaee5c865
Ashraf-1508 <ayaanashraf69@gmail.com>
2024-01-25 23:42:43 +0530

    Checked by TA

--- codes/plot.py
+++ codes/plot.py
@@ -1,16 +1,17 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-# Generate data points
-n_values = list(range(0,22))
-y_values = [6 + 6 * n for n in n_values]
+# Generate data points using NumPy arrays
+n_values = np.arange(0, 22)
+y_values = 6 + 6 * n_values
 
 # Create a stem plot
-plt.stem(n_values, y_values, linefmt='-', markerfmt='o', basefmt=' ')
+plt.stem(n_values, y_values, linefmt='-', markerfmt='o', basefmt='')
 
 # Add labels and title
 plt.xlabel('n')
 plt.ylabel('6 + 6n')
-plt.title('Stem Plot of 6+6n vs n')
+plt.title('Stem Plot of x(n) vs n')
 
 # Display the plot
 plt.show()
