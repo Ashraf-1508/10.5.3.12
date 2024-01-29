0d15443024a17c54df9c06c615cdb5403dad737c
Ashraf-1508 <ayaanashraf69@gmail.com>
2024-01-29 20:30:49 +0530

    updated

--- codes/plot.py
+++ codes/plot.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
-# Generate data points using NumPy arrays
-n_values = np.arange(0, 22)
-y_values = 6 + 6 * n_values
+# Read values from a.dat file
+with open("a.dat", "r") as file:
+    data = [line.split() for line in file]
+
+# Extract x and y values
+x_values = [int(row[0]) for row in data]
+y_values = [float(row[1]) for row in data]
 
 # Create a stem plot
-plt.stem(n_values, y_values, linefmt='-', markerfmt='o', basefmt='')
+plt.stem(x_values, y_values, linefmt='-', markerfmt='o', basefmt='')
 
 # Add labels and title
 plt.xlabel('n')
@@ -15,3 +18,4 @@ plt.title('Stem Plot of x(n) vs n')
 
 # Display the plot
 plt.show()
+
